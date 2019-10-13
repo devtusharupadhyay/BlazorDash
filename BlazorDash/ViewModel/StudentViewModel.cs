d2c201ccb5601edc26177723a07ba11f6c42dc59
Abhisehk Sharma <abhi.sharma5508@gmail.com>
2019-10-13 16:00:23 +0530

    pagination bug resolved

---
+++ BlazorDash/ViewModel/StudentViewModel.cs
@@ -0,0 +1,14 @@
+﻿using BlazorDash.Models;
+using BlazorDash.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorDash.ViewModel
+{
+    public class StudentViewModel
+    {
+        public PaginatedList<Student> Students { get; set; }
+    }
+}
